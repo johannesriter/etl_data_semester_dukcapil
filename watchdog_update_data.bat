6ea152a45dedb39af28bd1e2a7bdd7fc0e897ffc
johannesriter <johannesriter@gmail.com>
2023-06-27 13:28:39 +0700

    initial commit.

---
+++ watchdog_update_data.bat
@@ -0,0 +1,3 @@
+@echo off
+TITLE WATCHDOG_UPDATE_DATA_SEMESTER
+"C:\Users\Administrator\AppData\Local\ESRI\conda\envs\arcgispro-py3-clone\python.exe" "D:\etl_data_semester_dukcapil\watchdog_update_data.py"
---
+++ watchdog_update_data.py
@@ -0,0 +1,64 @@
+#!/home/arcgis/miniconda3/bin/python
+
+import arcpy
+import datetime
+import sys
+import time
+import logging
+import os
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
+from watchdog.observers.polling import PollingObserver
+
+monitor_folder = r'D:\test_update_folder'
+
+class ExampleHandler(FileSystemEventHandler):
+    def on_created(self, event): # when file is created
+        fullstring =  event.src_path
+        substring = '.csv'
+        print("Current file: {}".format(fullstring))
+        namafile = os.path.basename(fullstring)
+        
+        if substring in os.path.basename(namafile):
+            print('process data')
+            try :
+                python_path = r'C:\Users\Administrator\AppData\Local\ESRI\conda\envs\arcgispro-py3-clone\python.exe' #r'/home/arcgis/miniconda3/bin/python'
+                script_arcgis = r'D:\etl_data_semester_dukcapil\ETL_update_data_semester.py' #r'/home/arcgis/monitor_folder_SPH/update_attachment_SPH.py'
+                arguments = ('%s %s'%(python_path, script_arcgis))
+                os.system(arguments)
+                print('Now processing data {}'.format(fullstring))
+                print('Complete, waiting for next data')
+            
+            except Exception as e:
+                print(e)
+                sys.exit(1)
+
+        else:
+            print ('it is not the exact data')
+            pass
+        
+        # os.remove(fullstring)
+        
+logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+# path = monitor_folder #(ORIGINAL)
+path = sys.argv[1] if len(sys.argv) > 1 else '.'
+observer = PollingObserver() #Observer()
+event_handler = ExampleHandler() # create event handler (ORIGINAL)
+# event_handler = LoggingEventHandler() # create event handler
+
+# set observer to use created handler in 
+print ('Start monitoring folder ...')
+observer.schedule(event_handler, path, recursive=True)
+observer.start()
+
+# sleep until keyboard interrupt, then stop + rejoin the observer
+try:
+    while True:
+        # time.sleep(0.00000001) #(ORIGINAL)
+        time.sleep(5)
+except KeyboardInterrupt:
+    print ('Shutting down monitoring folder system')
+    observer.stop()
+observer.join()
